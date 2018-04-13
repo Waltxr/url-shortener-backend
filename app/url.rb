97d376c25aa3f7ec7f32f1f2b4d8533cd2efb02e
Adam Walter <adam.iverson.walter@gmail.com>
2018-04-13 16:47:47 -0400

    Added custom validation to URL model by checking if url is an URI::HTTP object.

---
+++ app/url.rb
@@ -0,0 +1,23 @@
+class Url < ApplicationRecord
+  validates :decoded_url, presence: true, url: true
+
+  # bijective function that encodes integer into base62 string
+  # shuffeld characters of lowercase alphabet + uppercase alphabet + 0 -9
+  CHARACTERS = 'rtYsk3i201XpTa5EyqGKQS6Mwu8BZmve7b9DOzNAfVd4FxHLCIUnhloRcJgWjP'.split(//)
+
+  def create_slug(id)
+    s = ''
+    base = CHARACTERS.length
+    while id > 0
+      s << CHARACTERS[id.modulo(base)]
+      id /= base
+    end
+    self.slug = s.reverse
+    self.save
+  end
+
+  def increment_access_count
+    self.access_count += 1
+  end
+
+end
