925e54502b4b1b6a943aa6b7990544dcb4cf1e0c
Adam Walter <Waltxr@users.noreply.github.com>
2018-04-15 14:06:56 -0400

    slight refactoring of url validation.

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -12,8 +12,9 @@ class UrlValidator < ActiveModel::EachValidator
   end
 
   def valid_url?(url)
+    # credit for the regex https://stackoverflow.com/questions/1805761/how-to-check-if-a-url-is-valid
     url_regexp = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
-    url =~ url_regexp ? true : false
+    !!(url =~ url_regexp)
   end
 
 end
